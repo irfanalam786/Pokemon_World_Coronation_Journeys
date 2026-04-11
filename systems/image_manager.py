e9364097e57b5a5324b585e16f7442a4cf1b04f6
IRFAN ALAM <mr.irfan.alam.429529@gmail.com>
2026-04-11 08:39:21 +0530

    Auto update

--- systems/image_manager.py
+++ systems/image_manager.py
@@ -1,19 +1,12 @@
-import os
+import requests
 from PIL import Image
+from io import BytesIO
 
 class ImageManager:
-    def __init__(self):
-        self.base_path = "assets/images"
-
-    def show(self, image_name):
-        path = os.path.join(self.base_path, image_name)
-
-        if not os.path.exists(path):
-            print(f"[Image Missing] {image_name}")
-            return
-
+    def show(self, image_url):
         try:
-            img = Image.open(path)
+            response = requests.get(image_url)
+            img = Image.open(BytesIO(response.content))
             img.show()
         except Exception as e:
-            print(f"[Error displaying image]: {e}")
+            print(f"[Image Error]: {e}")
